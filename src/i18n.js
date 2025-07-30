06f2762850034a2ed38590d44b5f0a0dd471b071
MoWagih <mowagih2@gmail.com>
2025-07-30 20:24:27 +0300

    updates-11

--- src/i18n.js
+++ src/i18n.js
@@ -2,16 +2,16 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import translationAR from "./locales/ar/translation.json";
-import translationEN from "./locales/en/translation.json";
+import ar from "./locales/ar/translation.json";
+import en from "./locales/en/translation.json";
 
 i18n
   .use(LanguageDetector) // يخلي الموقع يلتقط لغة المتصفح لو حابب
   .use(initReactI18next)
   .init({
     resources: {
-      ar: { translation: translationAR },
-      en: { translation: translationEN },
+      ar: { translation: ar },
+      en: { translation: en },
     },
     lng: "ar",            
     fallbackLng: "ar",   
